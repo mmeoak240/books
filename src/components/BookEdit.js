7f827478b77c94368f5a4131b83058c857d9269e
Michael Meoak <mmeoak240@gmail.com>
2023-07-11 16:36:18 -0700

    Full project commit

---
+++ src/components/BookEdit.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import useBooksContext from "../hooks/use-books-context";
+
+function BookEdit({ book, onSubmit }) {
+	const [title, setTitle] = useState(book.title);
+	const { editBookById } = useBooksContext();
+
+	const handleChange = (e) => {
+		setTitle(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onSubmit();
+		editBookById(book.id, title);
+	};
+	return (
+		<form className="book-edit" onSubmit={handleSubmit}>
+			<label> Title</label>
+			<input className="input" value={title} onChange={handleChange} />
+			<button className="button is-primary">Save</button>
+		</form>
+	);
+}
+
+export default BookEdit;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "./context/books";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Provider value={5}>
+		<App />
+	</Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
